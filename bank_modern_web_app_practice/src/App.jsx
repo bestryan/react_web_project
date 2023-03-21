ff5c49b2b405e9de4dae087af6f74124e9dc37d8
Ryan Xiao <77907899+bestryan@users.noreply.github.com>
2023-03-21 22:48:49 +1100

    Nav and Hero
    style Nav and Hero section

--- bank_modern_web_app_practice/src/App.jsx
+++ bank_modern_web_app_practice/src/App.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import styles from './style';
-
 import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
 
 const App = () => {
