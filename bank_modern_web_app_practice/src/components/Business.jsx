bf21638ea8f6e05724d485c0d12f74717df39510
Ryan Xiao <77907899+bestryan@users.noreply.github.com>
2023-03-23 15:09:18 +1100

    product section

--- bank_modern_web_app_practice/src/components/Business.jsx
+++ bank_modern_web_app_practice/src/components/Business.jsx
@@ -1,9 +1,20 @@
 import { features } from "../constants";
-import styles, {layout} from "../style";
+import styles, { layout } from "../style";
 import Button from './Button';
 
-const FeatureCard = () => (
-  <div>
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? 'mb-6' : 'mb-0'} feature-card`}>
+    <div className={`w-[64px] h-[64px] ${styles.flexCenter} bg-dimBlue rounded-full`}>
+      <img src={icon} alt='icon' className="w-[50%] h-[50%] object-contain"/>
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-semibold text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
+    </div>
 
   </div>
 )
@@ -11,17 +22,17 @@ const FeatureCard = () => (
 
 const Business = () => {
 
-
   return (
     <section id="features" className={layout.section}>
       <dv className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>You do the business, <b className="sm:block hidden"/>we’ll handle the money.</h2>
+        <h2 className={styles.heading2}>You do the business, <b className="sm:block hidden" />we’ll handle the money.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
-        <Button styles='mt-10'/>
+        <Button styles='mt-10' />
       </dv>
+
       <div className={`${layout.sectionImg} flex-col`}>
         {features.map((feature, index) => (
-          <FeatureCard key={feature.id} {...feature} index={index}/>
+          <FeatureCard key={feature.id} {...feature} index={index} />
         ))}
 
       </div>
